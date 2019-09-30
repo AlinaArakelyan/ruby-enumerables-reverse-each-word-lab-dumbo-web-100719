eeeee55d6b873bf203b2dc9e016a66a0449632bb
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-30 16:48:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,4 +8,11 @@ rev_array.join(' ')
 end
 
 
-
+def reverse_each_word(string)
+  array = string.split(" ")
+  rev_array = []
+  array.collect do |index|
+  rev_array << index.reverse
+  end
+rev_array.join(' ')
+end
